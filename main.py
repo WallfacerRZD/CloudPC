5d9ff88161419da3a9a8db353448010b928fdec8
WallfacerRZD <1048058400@qq.com>
2017-10-24 18:12:26 +0800

    optimize

--- main.py
+++ main.py
@@ -5,53 +5,70 @@ import time
 import mail_sender
 from PIL import ImageGrab
 
-
 screen_path = 'screen.jpg'
 # 邮箱地址
 your_email_adr = 'xxxxxxx'
 # 口令
 your_password = 'xxxxxxx'
 
+
 def shoot_screen():
     screen = ImageGrab.grab()
     screen.save(screen_path)
 
 
-def get_mail_count_and_content():
-    email = '1048058400@qq.com'
+def login():
+    addr = '1048058400@qq.com'
     # 口令
     password = "tuhnimgbsntgbecg"
     # pop服务器
     pop_server = 'pop.qq.com'
     # 连接到pop服务器
     server = poplib.POP3_SSL(pop_server, port=995)
-    server.user(email)
+    server.user(addr)
     server.pass_(password)
-    mail_count = server.stat()[0]
-    content = server.retr(mail_count)[1][0:10]
-    mail_content = '\r\n'.join(content)
-    return (mail_count, mail_content)
+    return server
+
+
+def get_mail_count():
+    server = login()
+    cnt = server.stat()[0]
+    server.quit()
+    return cnt
+
+
+def get_mail_content(index):
+    server = login()
+    try:
+        content = ''.join(server.retr(index)[1][:10])
+    except Exception, e:
+        print e
+        "发送邮件提示用户"
+        content = ""
+    finally:
+        server.quit()
+    return content
 
 
 pattern = re.compile('.*?Subject:(.*?)Mime-Version.*?', re.S)
+
 print 'running....'
-old_count = get_mail_count_and_content()[0]
+old_cnt = get_mail_count()
 while True:
-    time.sleep(5)
-    current_count, msg_content = get_mail_count_and_content()
-    if current_count != old_count:
-        index = current_count
-        match_list = pattern.findall(msg_content)
+    time.sleep(1)
+    cnt = get_mail_count()
+    print cnt
+    if cnt != old_cnt:
+        content = get_mail_content(cnt)
+        match_list = pattern.findall(content)
         instruction = match_list[0].strip() if match_list else ''
+        print instruction
         if instruction == 'shotscreen':
             shoot_screen()
-            mail_sender.send(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), 'plain', image_path=screen_path)
+            mail_sender.send(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())), 'plain',
+                             image_path=screen_path)
         elif instruction == '':
             pass
         else:
             print u'未识别的指令'
-        old_count = current_count
-
-
-
-
+        old_cnt = cnt
